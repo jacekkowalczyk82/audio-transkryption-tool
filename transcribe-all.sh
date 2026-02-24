07494d926c59f6c35f7fa3627ad61ed5a67f28c3
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2026-02-24 21:33:44 +0100

    added scripts

---
+++ transcribe-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Initialize conda for this shell session
+# This ensures 'conda activate' works even if the script is run non-interactively
+eval "$(conda shell.bash hook)"
+
+# Activate the specific environment
+conda activate dev-audio-transkryption-tool
+
+# Run the application, passing all arguments ("$@") to it
+python main.py --cli  --dir /home/jacek/dyktafon/
+
